1b313979e96f16f0470e2b2c9ce0576ecb14ddb3
Zerth Monk <26793601+zerthmonk@users.noreply.github.com>
2019-12-11 13:13:12 +0200

    start application

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 import os
-import yaml
 
 import skabenclient as client
 from handler import LockHandler
-from lock import LockBox
+from device import LockDevice
+
+config = client.helpers.get_config()
+
+device = LockDevice(config)
+handler = LockHandler(config)
+
+client.start_app(app_config=config,
+                 device_handler=device, 
+                 event_handler=handler)
