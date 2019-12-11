4c8cf0a9f4a63fd69bcc4014aa117dd17fa54c93
Zerth Monk <26793601+zerthmonk@users.noreply.github.com>
2019-12-11 13:19:04 +0000

    minor fix

--- app.py
+++ app.py
@@ -10,6 +10,6 @@ config = client.helpers.get_config(conf_path)
 device = LockDevice(config)
 handler = LockHandler(config)
 
-client.start_app(app_config=config,
+client.start_app(config=config,
                  device_handler=device,
                  event_handler=handler)
