e7bafe2cc98d8fd1b1b3aeefe489e6c025b7b54b
Zerth Monk <26793601+zerthmonk@users.noreply.github.com>
2019-12-12 15:17:22 +0000

    versioning

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os.path as op
 from skabenclient.config import Config
 
-config = Config()
+config = Config(op.join('conf', 'config.yml'))
 config.update({
     'sound_dir': op.join(config.root, 'resources', 'sound'),
 })
