c9dfe49bd8b6787759443bd5cc5b8af19c6ebb9d
Zerth Monk <26793601+zerthmonk@users.noreply.github.com>
2020-01-16 16:14:40 +0300

    app root fixed

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ from skabenclient.main import start_app
 
 from device import LockDevice
 
-root = os.path.dirname(os.path.abspath(__file__))
+root = os.path.abspath(os.path.dirname(__file__))
 app_config_path = os.path.join(root, 'conf', 'config.yml')
 
 if __name__ == "__main__":
-    app_config = SystemConfig(app_config_path)
+    app_config = SystemConfig(app_config_path, root=root)
     # default device config file is conf/device.yml
     device = LockDevice(app_config)
     device.gpio_setup()  # pins for laser control and serial interface for keypads
