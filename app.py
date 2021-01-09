01d11cf5850f2bfc471d62691e8114921170e0e9
Zerth Monk <26793601+zerthmonk@users.noreply.github.com>
2021-01-09 13:21:12 +0200

    logger invocation

--- app.py
+++ app.py
@@ -10,17 +10,12 @@ root = os.path.abspath(os.path.dirname(__file__))
 
 sys_config_path = os.path.join(root, 'conf', 'system.yml')
 dev_config_path = os.path.join(root, 'conf', 'device.yml')
-log_path = os.path.join(root, 'local.log')
 
 if __name__ == "__main__":
     # setting up system configuration and logger
     app_config = SystemConfig(sys_config_path)
-    app_config.logger(file_path=log_path)
-    # instantiating device
     dev_config = LockConfig(dev_config_path)
     device = LockDevice(app_config, dev_config)
-    # perform lock-specific actions on device
     device.gpio_setup()  # pins for laser control and serial interface for keypads
-
     start_app(app_config=app_config,
               device=device)
