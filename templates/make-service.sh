f3e003992822d4c9726231c0000cdc7002e9e2a7
Zerth Monk <26793601+zerthmonk@users.noreply.github.com>
2021-05-10 19:53:13 +0300

    minor fixes

--- templates/make-service.sh
+++ templates/make-service.sh
@@ -3,9 +3,11 @@
 SCRIPT=`readlink -f "$0"`
 SCRIPTPATH=`dirname "$SCRIPT"`
 SKABEN_ROOT_DIR=`dirname "$SCRIPTPATH"`
+SERVICE_FILE="$SKABEN_ROOT_DIR/skabenlock.service.tmp"
 
-sed -e "s+\${dirpath}+$SKABEN_ROOT_DIR+" "$SKABEN_ROOT_DIR/templates/lock.service.template" > "$SKABEN_ROOT_DIR/skabenlock.service.tmp"
-mv $SKABEN_ROOT_DIR/skabenlock.service.tmp /etc/systemd/system/skabenlock.service
+touch $SERVICE_FILE
+sed -e "s+\${dirpath}+$SKABEN_ROOT_DIR+" "$SKABEN_ROOT_DIR/templates/lock.service.template" >$SERVICE_FILE
+cp $SERVICE_FILE /etc/systemd/system/skabenlock.service
 
 systemctl daemon-reload
 systemctl disable newlock &> /dev/null
